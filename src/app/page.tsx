25aac87ab6b9502a942601271b1253e4b61c787d
Itai Rotstein <itairotstein@gmail.com>
2025-03-07 13:17:57 +0300

    added suspense to DestinationSearch when called from homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,12 @@
 import DestinationSearch from "@/components/DestinationSearch";
+import { Suspense } from "react";
 
 export default function Home() {
   return (
     <main className="p-4">
-      <DestinationSearch />
+      <Suspense fallback={<div>Loading...</div>}>
+        <DestinationSearch />
+      </Suspense>
     </main>
   );
 }
